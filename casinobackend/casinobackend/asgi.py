4c43669507a02853c4ed0c1ee9a69158e7c14bf4
VARAD819 <bhandarivarad09@gmail.com>
2023-12-17 23:53:20 +0530

    hypercorn

--- casinobackend/casinobackend/asgi.py
+++ casinobackend/casinobackend/asgi.py
@@ -8,15 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'casinobackend.settings')
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
-})
+async def application(scope, receive, send):
+    await django_asgi_app(scope, receive, send)
